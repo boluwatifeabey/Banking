33a5ab6dd4ad6b63120b5115dee5ec60859bd95f
abiodun <boluwatifeabey21@gmail.com>
2024-09-17 10:10:00 +0100

    implement initial file and folder structure

---
+++ app/layout.tsx
@@ -0,0 +1,45 @@
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif } from "next/font/google"
+// import localFont from "next/font/local";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"], variable: '--font-inter'});
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-imb-plex-serif'
+})
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
+
+export const metadata: Metadata = {
+  title: "Horizon",
+  description: "Horizon is a modern banking Platfrom for everyone.",
+  icons: {
+    icon: '/icons/logo.svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.variable} ${ibmPlexSerif.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
